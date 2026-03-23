725fe09169e59f39e4aefb956db5a5fc732b67d8
= <=>
2026-03-23 20:29:02 +0400

    feat: implement assets model entities

---
+++ Models/Asset.cs
@@ -0,0 +1,12 @@
+﻿namespace FintachartsAPI.Models
+{
+    public class Asset
+    {
+        public Guid Id { get; set; }
+        public Guid FintachartsId { get; set; }
+        public string Symbol { get; set; } = string.Empty;
+        public string Provider { get; set; } = string.Empty;
+        public string Kind { get; set; } = string.Empty;
+        public ICollection<AssetPrice> Prices { get; set; } = new List<AssetPrice>();
+    }
+}
---
+++ Models/AssetPrice.cs
@@ -0,0 +1,11 @@
+﻿namespace FintachartsAPI.Models
+{
+    public class AssetPrice
+    {
+        public Guid Id { get; set; }
+        public Guid AssetId { get; set; }
+        public Asset? Asset { get; set; }
+        public decimal Price { get; set; }
+        public DateTime TimeStamp { get; set; }
+    }
+}
