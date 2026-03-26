efe5924cefdc8f4a8082fa9d6d872296ff85aba8
= <=>
2026-03-26 16:25:47 +0400

    feat: add Docker Compose orchestration and EF Core auto-migrations on startup

---
+++ src/Extensions/WebApplicationExtensions.cs
@@ -0,0 +1,15 @@
+﻿using FintachartsAPI.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace FintachartsAPI.Extensions
+{
+    public static class WebApplicationExtensions
+    {
+        public static async Task ApplyDatabaseMigrationsAsync(this WebApplication app)
+        {
+            using var scope = app.Services.CreateScope();
+            var context = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+            await context.Database.MigrateAsync();
+        }
+    }
+}
